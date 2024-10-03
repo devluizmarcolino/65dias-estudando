50352063755dea176c3f3bd19bb0776c9c6d081a
devluizmarcolino <devluizmarcolino@gmail.com>
2024-10-03 15:08:45 -0300

    :star: 50 min de estudos full java script (dia2)

---
+++ dia 2/assets/app.js
@@ -0,0 +1,30 @@
+// ! tipos de variáveis:
+let idade = 19; // ! tipo number
+let nome = "Luiz"; // ! tipo string
+let logado = true; // ! tipo boolean
+
+// * array e objetos:
+let ingredientes = ["farinha", "água", "sal", "corante"]; // * array
+let personagem = {
+  nome: "Luizão",
+  nivel: 33,
+  forca: 103,
+  magia: 113,
+  vida: 10003,
+  mana: 32341,
+}; // * objeto
+
+// ? function
+
+// ? function somar(a, b) {
+// ?  let resultado = a + b;
+// ?  return resultado
+// ? }   function normal
+
+// ? let somar2 = (a, b) => {
+// ?  let resultado = a + b;
+// ?  return resultado
+// ? } arrow function
+
+// ? let x = somar(15, 50)
+// ? let y = somar1(2, 8)
